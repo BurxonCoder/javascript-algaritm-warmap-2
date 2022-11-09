208866711684d2701d96bb81670e974b8ea46029
Burxon Fozilov <113450585+BurxonCoder@users.noreply.github.com>
2022-11-09 12:12:48 +0500

    jrjfri

--- main.js
+++ main.js
@@ -6,13 +6,6 @@
 // stringTimes("Hi", 3) → "HiHiHi"
 // stringTimes("Hi", 1) → "Hi"
 
-// function stringTime (str , n) {
-    
-// let str1 = str.slice(0, n)
-// let str2 = str.slice(n + 1,)
-
-// return  str1 + str2 + str 
-// }
 
 function  stringTime(str , n) {
 let str1 = ''
